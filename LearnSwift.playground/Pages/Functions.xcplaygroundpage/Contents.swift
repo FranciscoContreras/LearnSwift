21950ef38921142956c42d461e69cdb1e5f68ded
FranciscoContreras <Contrerasfrancisco2010@gmail.com>
2022-11-08 14:23:03 -0800

    Working with more advanced functions

--- LearnSwift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ LearnSwift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -57,3 +57,88 @@ someFunction(firstParameterName: 3, secondParameterName: 6)
 
 someFunction(parameterWithoutDefault: 4)
 
+
+//
+// Advanced; Function Types
+//
+
+// Every function has a specific type, made up of the parameter types and the return type of the function
+
+//func addTwoInts(_ a: Int, _ b: Int) -> Int{
+//    return a + b
+//}
+
+func multiplyTwoInts(_ a: Int, _ b: Int) -> Int{
+    return a * b
+}
+
+// The type of both of these functions is (Int, Int) -> Int. This can be read as:
+//
+// "A function that has two parameters, both of type Int, and that returns a value of type Int."
+
+// Here's another example, for the function with no parameters or return value:
+
+func printHelloWorld() {
+    print("Hello, world")
+}
+
+
+//Note: The type of this functions is () -> Void,
+//      or "a functions that has no parameters, and return Void."
+//      Which means even though there is no return, the type is still of type void.
+
+
+// Using Function Types
+
+// Functions can be assigned types.
+
+func addTwoInts(_ a: Int, _ b: Int) -> Int{
+    return a + b
+}
+
+addTwoInts(20, 20)
+
+var mathFunction: (Int, Int) -> Int = addTwoInts
+
+// Functions as parameters
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+
+func lessThanTen(number: Int) -> Bool{
+    return number < 10
+}
+
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
+
+
+// - Ready Player 1
+//
+// Write a function for above matchers that returns true if any numbers between 1 and 10(inclusive)
+//
+// [20, 19, 7, 12]
+// [20, 19, 12]
+
+func betweenOneAndTen(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+
+func checkNumber(number: Int) -> Bool{
+    return number <= 10;
+}
+
+
+var numbersTwo = [20, 19, 12]
+betweenOneAndTen(list: numbersTwo, condition: checkNumber)
